8134346bc342940b366030326614fd92d5696723
J4nis05 <janis.roelli@gmail.com>
2024-01-11 15:11:08 +0100

    Added 2

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ def git_push():
 
 
 def get_count():
-    with open('count.txt', 'r') as file:
+    with open('/scripts/autocommiter/count.txt', 'r') as file:
         number = file.read()
     return int(number)
 
 
 def add_count(number):
     new_number = number + 1
-    with open('count.txt', 'w') as f:
+    with open('/scripts/autocommiter/count.txt', 'w') as f:
         f.write(str(new_number))
     return f"Count increased by 1. It is now at {new_number}."
 
 
 def get_line(line_number):
-    with open('source.txt', 'r') as file:
+    with open('/scripts/autocommiter/source.txt', 'r') as file:
         lines = file.readlines()
         # Check if the line number is valid
         if 1 <= line_number <= len(lines):
@@ -38,14 +38,15 @@ def get_line(line_number):
 
 
 def add_line(text):
-    with open('script.txt', 'a') as file:
+    with open('/scripts/autocommiter/script.txt', 'a') as file:
         file.write(f"{text}\n")
 
 
-repo = git.Repo('./')            # Set Repository Path
+repo = git.Repo('/scripts/autocommiter/')
+
 count = get_count()              # Get Current Script line
 current_text = get_line(count)   # Set Text to insert
 add_line(current_text)           # Add Text to Output file
 add_count(count)                 # Increase Count by 1
-git_commit(f"Now at {count}")    # Commit the changes
+git_commit(f"Added {count}")     # Commit the changes
 git_push()                       # Push the changes
